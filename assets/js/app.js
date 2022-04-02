2c0a3eda5024612c17ba3812571906c105cd81a8
ravi7483 <somwadvaibhav@gmail.com>
2022-04-02 09:43:26 +0530

    undefined notdefined
    main

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+console.log('app js file')
+
+var x =10;
+console.log(x); // 10
+
+var y;
+console.log(y); // undefined : the decl is defined but not any assign value
+
+var p;              // declration
+console.log(p);
+
+ p = 10;            // assign value
+ console.log(p);
+
+var z;            // undefined :- the decleration is defined but not assign any value to yet. 
+console.log(z); 
+ console.log(q); // not-defined : the declration is not-defined but not any assign value
+
+console.log(z); // not-defined : the decl is not defined and not assign value
+
